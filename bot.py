4f071eb0ffd6f394066dbf88480bc181f1a8de04
coder089 <117955242+coder089@users.noreply.github.com>
2023-12-22 00:46:41 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ import json
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
+
 if not 'Config.txt' in os.listdir():
     open('Config.txt', 'w').write('{"account_sid":"?", "auth_token":"?", "Twilio Phone Number":"+1?", "ngrok_url":"https://you-url.ngrok.io", "server_id":"?", "bot_token":"?"}')
 if not 'grabbed_otp.txt' in os.listdir():
